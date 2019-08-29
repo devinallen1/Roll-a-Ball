be475dbf3177772fccc1a8426eeea951063467c6
devinallen1 <devinallen1@mail.weber.edu>
2019-08-29 17:11:41 -0600

    First Commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    #region --PUBLIC VARIABLES-- 
+
+    public GameObject player;
+
+    #endregion
+
+    #region --PRIVATE VARIABLES-- 
+
+    private Vector3 offset; 
+
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position; 
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    #region --PUBLIC VARIABLES-- 
+    public float Speed;
+    public TextMeshProUGUI countText;
+    public TextMeshProUGUI winText;
+    #endregion
+
+    #region  --PRIVATE VARIABLES--
+    private Rigidbody rb;
+    private int count;
+    #endregion
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+    void Update()
+    {
+        SetWinText();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * Speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+    }
+
+    void SetWinText()
+    {
+        if (count == 12)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
